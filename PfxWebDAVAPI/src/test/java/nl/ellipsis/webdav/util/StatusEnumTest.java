01c9d5f699ae0f5d169befcd2392f80c9688bad3
ellipsisnl <info@ellipsis.nl>
2018-02-02 12:40:07 +0100

    use constants of existing libraries whenever possible

--- PfxWebDAVAPI/src/test/java/nl/ellipsis/webdav/util/StatusEnumTest.java
+++ PfxWebDAVAPI/src/test/java/nl/ellipsis/webdav/util/StatusEnumTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import nl.ellipsis.webdav.util.StatusEnum;
-import nl.ellipsis.webdav.v1.DAVHeader;
 import nl.ellipsis.webdav.v1.DAVStatus;
 
 public class StatusEnumTest {
@@ -39,17 +38,10 @@ public class StatusEnumTest {
 	}
 
 	@Test
-	public void testDAVHeader() {
-		StatusEnum<DAVHeader> e = StatusEnum.set(DAVHeader.IF);
-		assertEquals("If",e.message);
-		assertEquals(-1,e.code);
-	}
-
-	@Test
 	public void testDAVStatus() {
 		StatusEnum<DAVStatus> e = StatusEnum.set(DAVStatus.LOCKED_423);
 		assertEquals("Locked",e.message);
 		assertEquals(423,e.code);
-	}
+	} 
 
 }
